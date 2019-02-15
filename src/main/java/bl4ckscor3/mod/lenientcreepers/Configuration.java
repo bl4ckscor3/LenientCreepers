09f2b66f438093b56dd270eb13f007499183fc4b
bl4ckscor3 <donalddani9612@gmx.de>
2019-02-15 16:44:05 +0100

    update to 1.13.2

--- src/main/java/bl4ckscor3/mod/lenientcreepers/Configuration.java
+++ src/main/java/bl4ckscor3/mod/lenientcreepers/Configuration.java
@@ -1,13 +1,34 @@
 package bl4ckscor3.mod.lenientcreepers;
 
-import net.minecraftforge.common.config.Config;
-import net.minecraftforge.common.config.Config.Comment;
-import net.minecraftforge.common.config.Config.Type;
+import org.apache.commons.lang3.tuple.Pair;
 
-@Config(modid=LenientCreepers.MOD_ID, type=Type.INSTANCE, category="general")
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.common.ForgeConfigSpec.BooleanValue;
+
 public class Configuration
 {
-	@Comment({"If this is set to false, items will not be destroyed regardless of what the gamerule mobGriefing is set to",
-	"Default: true"})
-	public static boolean onlyWithMobGriefingGamerule = true;
+	public static final ForgeConfigSpec CONFIG_SPEC;
+	private static final Configuration CONFIG;
+
+	public final BooleanValue onlyWithMobGriefingGamerule;
+
+	static
+	{
+		Pair<Configuration,ForgeConfigSpec> specPair = new ForgeConfigSpec.Builder().configure(Configuration::new);
+
+		CONFIG_SPEC = specPair.getRight();
+		CONFIG = specPair.getLeft();
+	}
+
+	Configuration(ForgeConfigSpec.Builder builder)
+	{
+		onlyWithMobGriefingGamerule = builder
+				.comment("If this is set to false, items will not be destroyed regardless of what the gamerule mobGriefing is set to")
+				.define("onlyWithMobGriefingGamerule", true);
+	}
+
+	public static boolean onlyWithMobGriefingGamerule()
+	{
+		return CONFIG.onlyWithMobGriefingGamerule.get();
+	}
 }
