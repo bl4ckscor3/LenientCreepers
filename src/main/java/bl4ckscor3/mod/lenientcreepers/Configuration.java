ec2f34e4a87c44807be203c4150f8c99911f4f63
bl4ckscor3 <bl4ckscor33@gmail.com>
2025-07-16 16:24:43 +0200

    Support the config screen

--- src/main/java/bl4ckscor3/mod/lenientcreepers/Configuration.java
+++ src/main/java/bl4ckscor3/mod/lenientcreepers/Configuration.java
@@ -1,8 +1,9 @@
 package bl4ckscor3.mod.lenientcreepers;
 
+import org.apache.commons.lang3.tuple.Pair;
+
 import net.neoforged.neoforge.common.ModConfigSpec;
 import net.neoforged.neoforge.common.ModConfigSpec.BooleanValue;
-import org.apache.commons.lang3.tuple.Pair;
 
 public class Configuration {
 	public static final ModConfigSpec CONFIG_SPEC;
@@ -19,7 +20,7 @@ public class Configuration {
 	Configuration(ModConfigSpec.Builder builder) {
 		//@formatter:off
 		onlyWithMobGriefingGamerule = builder
-				.comment("If this is set to false, items will not be destroyed regardless of what the gamerule mobGriefing is set to")
+				.comment("If this is set to false, items will not be destroyed regardless of what the gamerule mobGriefing is set to.")
 				.define("onlyWithMobGriefingGamerule", true);
 		//@formatter:on
 	}
